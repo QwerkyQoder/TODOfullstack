2b40d43f6dbb949f5abc8b90ba2b1df51881926a
Meera M <colorcodedcrayons@gmail.com>
2023-01-04 16:45:53 +0530

    Component files reorganized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Form from './components/Form';
+// import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div>
-      <Form/>
       <TodoList/>
     </div>
   );
--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -60,8 +60,43 @@ const TodoList = () => {
     getdata();
 }
 
+const [todo, setTodo] = useState("")
+    const[task, setTask] = useState("")
+
+    const submitData = async () => {
+        const data  = {
+            title: todo,
+            tasks: task
+        };
+        const res = await axios.post("/createTodo", data)
+        console.log(res)
+        getdata()
+    };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        submitData();
+        setTodo("");
+        setTask("");
+    }
+
   return (
 <div class="container">
+<div>
+        <form onSubmit={handleSubmit}>
+  <div class="m-5">
+    <label for="exampleInputTodo1" class="form-label">Todo Title</label>
+    <input type="text" class="form-control" id="exampleInputTodo1"
+    value={todo} onChange={(event) => setTodo(event.target.value)} />
+  </div>
+  <div class="m-5">
+    <label for="exampleInputTask1" class="form-label">Task</label>
+    <input type="text" class="form-control" id="exampleInputTaskword1"
+    value={task} onChange={(event) => setTask(event.target.value)} />
+  </div>
+  <button type="submit" class="btn btn-primary m-5">Submit</button>
+</form>
+  </div>
   <div class="row">
     <div class="col-12">
       <p></p>
